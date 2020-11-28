a3d0b2fa01dc95c7436929a35ad042545bc8487f
vinigarcia <vingarciasc@gmail.com>
2020-11-28 11:32:05 -0300

    extracting metrics from networks

--- reader.py
+++ reader.py
@@ -1,18 +1,17 @@
-import pdb
 import networkx as nx
+import numpy as np
 import matplotlib.pyplot as plt
-from collections import Counter
+import pandas as pd
+import pdb
+import json
 
-STOP_WORD_GROUPS = ["PRP", "VIRG", "SENT", "PR", "CONJ", "DET", "QUOTE", "PRP+DET"]
+from os import listdir
+from os.path import join
+from collections import Counter
+from csv import QUOTE_ALL
+
+STOP_WORD_GROUPS = ["PRP", "VIRG", "SENT", "PR", "CONJ", "DET", "QUOTE", "PRP+DET", "CARD"]
 STOP_WORD_LEMMAS = ["<unknown>"]
-FILENAMES = [
-	"data/abreu1856.txt",
-	"data/aires1752.txt",
-	"data/alencar1857.txt",
-	"data/alencar1865.txt",
-	"data/alencar1875.txt",
-	"data/almeida1633.txt",
-]
 
 def add_node(graph, word):
 	if not graph.has_node(word):
@@ -52,14 +51,60 @@ def read_colonia_file(filename):
 	return G, word_counter
 	
 if __name__ == '__main__':
-	for filename in FILENAMES:
+	metadata_df = pd.read_csv("colonia_metadata.csv")
+	path = "data/txt_colonia/"
+
+	should_write_graph = False
+	should_draw_graph = False
+	should_collect_metrics = False
+
+	data = {
+		"number_of_nodes": [],
+		"number_of_edges": [],
+		"density": [],
+		"assortativity_coefficient": [],
+		"mean_degree": [],
+		"mean_degree_centrality": [],
+		"max_degree_centrality": [],
+		"mean_clustering": [],
+		"max_clustering": [],
+		"average_shortest_path_length": []
+	}
+
+	for index, row in metadata_df.iterrows():
+		filename = join(path, row['filename'])
 		G, word_counter = read_colonia_file(filename)
 
 		print("--------")
 		print("filename", filename)
-		print("n:", G.number_of_nodes())
-		print("m:", G.number_of_edges())
-		print("20 most common", word_counter.most_common(20))
+
+		if should_write_graph:
+			graph_filename = "data/gml_colonia/" + row['filename'].split(".")[0] + ".graphml"
+			nx.write_graphml(G, graph_filename)
+
+		if should_collect_metrics:
+			data["number_of_nodes"].append(G.number_of_nodes())
+			data["number_of_edges"].append(G.number_of_edges())
+			data["density"].append(nx.density(G))
+			data["assortativity_coefficient"].append(nx.degree_assortativity_coefficient(G, weight='weight'))
+			data["mean_degree"].append(np.mean([val for key, val in G.degree(weight='weight')]))
+			degree_centrality = [val for key, val in nx.degree_centrality(G).items()]
+			data["mean_degree_centrality"].append(np.mean(degree_centrality))
+			data["max_degree_centrality"].append(np.max(degree_centrality))
+			# betweenness_centrality = [val for key, val in nx.betweenness_centrality(G).items()]
+			# data["mean_betweenness_centrality"].append(np.mean(betweenness_centrality))
+			# data["max_betweenness_centrality"].append(np.max(betweenness_centrality))
+			clustering = [val for key, val in nx.clustering(G, weight='weight').items()]
+			data["mean_clustering"].append(np.mean(clustering))
+			data["max_clustering"].append(np.max(clustering))
+			data["average_shortest_path_length"].append(nx.average_shortest_path_length(G, weight='weight'))
+			# data["average_betwenness"].append(np.mean([val for key, val in nx.betweenness_centrality(G, weight='weight').items()]))
 		
-		# nx.draw(G, with_labels=True)
-		# plt.show()
+		if should_draw_graph:
+			nx.draw(G, with_labels=True)
+			plt.show()
+
+	if should_collect_metrics:
+		for key, val in data.items():
+			metadata_df[key] = val
+		metadata_df.to_csv('network_metrics.csv', sep=';', quoting=QUOTE_ALL)
